007c827167a4e5bfd87f1bc5f2e7e2206a311480
JamesNellis <thejamesnellis@gmail.com>
2025-08-12 23:34:19 -0400

    mac August 12 2025 23:34

--- sim.c
+++ sim.c
@@ -8,16 +8,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <unistd.h>
 
 
 typedef struct{
 	int hand[10];
 	int size;
-	int hidden;
-	int money;
-	int bet;
-	int eval[2];
+	char hidden;
+	int eval;
+	char hasAce;
 } Hand;
 
 typedef struct{
@@ -25,17 +24,14 @@ typedef struct{
 	int deckS;
 
 	Hand hands[3];
-	Hand publicHands[3];
 	int userHands;
 	int played;
 } Table;
 
 
-void drawCard(Table *table, int i);
+void drawCard(Table *table, Hand *hand);
 
 void initHandNegTwo(int hand[], int len);
-
-void updatePublicHands(Table *table);
 
 void initTable(Table *table);
 
@@ -43,7 +39,7 @@ void shuffleDeck(Table *table);
 
 void evalHand(Table *table, Hand *hand, int print);
 
-void promptChoice(Table *table, int i);
+int makeChoice(Table *table, Hand *hand);
 
 //temp:
 
@@ -51,6 +47,8 @@ void showHand(Hand *hand);
 
 void showTable(Table *table);
 
+
+int sleeptime = 0;
 int main(){
 	srand(time(NULL));
 	Table table;
@@ -59,22 +57,28 @@ int main(){
 
 	for (int j = 0; j < 2; j++){
 		for (int i = 0; i < 2; i++){
-			drawCard(&table, i);
+			drawCard(&table, &table.hands[i]);
 		}
 	}
+	
 
-	// updatePublicHands(table);
-	// evalHand(table, &table->publicHands[i], 1);
+
 	showTable(&table);
+
 	table.hands[0].hidden = FALSE;
 	showTable(&table);
 
-	while (table.publicHands[0].eval[0] < 17){
-		drawCard(&table, 0);
+
+
+	// while (table.hands[1].eval
+	// 	makeChoice(&table);
+
+	while ((table.hands[0].eval < 17) || (table.hands[0].eval == 17 && table.hands[0].hasAce)){
+		drawCard(&table, &table.hands[0]); // evals when draw
 		showTable(&table);
 	}
 
-
+	for (;makeChoice(&table, &table.hands[1]););
 
 	return 0;
 }
@@ -82,44 +86,72 @@ int main(){
 
 
 
+int makeChoice(Table *table, Hand *hand){
+	char choice[1];
+	int doublable = (hand->size == 2);
+	int splitable = (doublable) && (hand->hand[0] == hand->hand[1]) && (table->userHands = 1);
+	printf("(H)it, (S)tand%s%s?: ", ((doublable) ? ", (D)ouble" : ""), (splitable) ? ", Split (2)" : "");
+    fflush(stdout);
+    
+    scanf("%c", choice);
+
+    while (1){
+
+    	// 1: go again 2: 
+	    if (choice[0] == 'h'){
+	    	drawCard(table, hand);
+	    	showTable(table);
+	    	return 1;
+	    }
+	    else if (choice[0] == 's'){
+	    	return 0;
+	    }
+	    else if (choice[0] == 'd' && doublable){
+	    	drawCard(table, hand);
+	    	showTable(table);
+	    	// hand->bet *= 2;
+	    	return 0;
+	    }
+	    else if (choice[0] == '2' && splitable) {
+	    	table->userHands = 2;
+	    	table->hands[2].hand[0] = table->hands[0].hand[1];
+	    	hand->size--;
+	    	table->hands[2].size++;
+	    	// carry over bet
+	    	return 1;
+	    }
+	    printf("Poor input.\n");
+	    printf("(H)it, (S)tand%s%s?: ", ((doublable) ? ", (D)ouble" : ""), (splitable) ? ", Split (2)" : "");
+	    fflush(stdout);
+	    scanf("%c", choice);
+
+	 }
+
+}
+
+
 
 void initHandNegTwo(int hand[], int len){
-	for (int i = 0; i < len; i++){
+	for (int i 
+		= 0; i < len; i++){
 		hand[i] = -2;
 	}
 }
 
-void updatePublicHands(Table *table) {
-	for (int j = 0; j < 2; j++){
-		for (int i = 0; i < table->hands[j].size; i++){
-			table->publicHands[j].hand[i] = MIN(table->hands[j].hand[i], (table->hands[j].hidden && i == 0) ? -1 : 10);
-		}
-		table->publicHands[j].size = table->hands[j].size;
-	}
-
-}
 
 void initTable(Table *table){
 
+	for (int i = 0; i < 3; i++){
+		table->hands[i].hidden = FALSE;
+		initHandNegTwo(table->hands[i].hand, 10);
+		table->hands[i].size = 0;
+		table->hands[i].hasAce = 0;
+	}
+
+
 	table->hands[0].hidden = TRUE;
-	initHandNegTwo(table->hands[0].hand, 10);
-	table->hands[0].size = 0;
-
-	table->hands[1].hidden = FALSE;
-	initHandNegTwo(table->hands[1].hand, 10);
-	table->hands[1].size = 0;
-
-	table->hands[2].hidden = FALSE;
-	initHandNegTwo(table->hands[2].hand, 10);
-	table->hands[2].size = 0;
-
 	table->userHands = 1;
 
-	for (int j = 0; j < 2; j++){
-		for (int i = 0; i < table->hands[0].size; i++){
-			table->publicHands[j].hand[i] = -2;
-		}
-	}
 
 	shuffleDeck(table);
 }
@@ -131,6 +163,7 @@ void shuffleDeck(Table *table){
 		table->deck[i+12] = MAX(0, (i / 4));
 	}
 
+
 	for (int i = 0; i < table->deckS; i++){
 		int randi = rand() % (table->deckS - i) + i;
 		int temp = table->deck[randi];
@@ -139,26 +172,21 @@ void shuffleDeck(Table *table){
 	}
 }
 
-void drawCard(Table *table, int i){
-	table->hands[i].hand[table->hands[i].size] = table->deck[table->deckS-1];
-	table->hands[i].size++;
+void drawCard(Table *table, Hand *hand){
+	hand->hand[hand->size] = table->deck[table->deckS-1];
+	hand->size++;
 	table->deckS--;
-	updatePublicHands(table);
-	evalHand(table, &table->publicHands[i], 0);
+	evalHand(table, hand, 0);
 }
 
 void evalHand(Table *table, Hand *hand, int print) {
 
-	// no ace, no bust, no bj: n, -1
-	// ace: n, n+10
-	// bj: 21, -1
-	// bust: -2, max([0:1])
 
-	hand->eval[1] = -1;
+	hand->eval = -1;
 	int sum = 0;
 	int ace = FALSE;
 
-	for (int i = 0; i < hand->size; i++){
+	for (int i = (hand->hidden? 1 : 0); i < hand->size; i++){
 
 		// add card value
 		if (hand->hand[i] != -1) {
@@ -170,23 +198,18 @@ void evalHand(Table *table, Hand *hand, int print) {
 				}	
 			}
 	}
-
-	hand->eval[0] = MAX(MIN(sum, 21), MIN(sum + (ace ? 10 : 0), 21));
-
-	if (print){
-		printf("EVAL: %d\n", MAX(MIN(sum, 21), MIN(sum + (ace ? 10 : 0), 21)));
-
+	if (ace) {
+		hand->hasAce = 1;
+	}
+	if (sum <= 21 && ace && sum < 12){
+		sum += 10;
 	}
 
-}
+	hand->eval = sum;
+	if (print){
+		printf("EVAL: %d\n", sum);
 
-
-void promptChoice(Table *table, int i){
-	char choice[1];
-	int doublable = table->hands[i].size == 2;
-	int splitable = doublable && (table->hands[i].hand[0] == table->hands[i].hand[1]);
-	printf("(H)it, (S)tand%s%s?: ", ((doublable) ? ", Double" : ""), (splitable) ? ", Split (2)" : "");
-    scanf("%c", choice);
+	}
 
 }
 
@@ -197,19 +220,21 @@ void promptChoice(Table *table, int i){
 //temp for tetsing:
 
 void showHand(Hand *hand){
-	for (int i = 0; i < hand->size; i++){
+	printf("%s", hand->hidden? "X, ": "");
+	for (int i = (hand->hidden? 1: 0); i < hand->size; i++){
 		printf("%d, ", hand->hand[i]);
 	}
 	printf("\n");
 }
 
 void showTable(Table *table){
-	updatePublicHands(table);
+	system("clear");
 	printf("Table's Hands: \n");
 	for (int i = 0; i < (table->userHands + 1); i++){
 		printf("Hand %d: \n", i);
-		showHand(&(table->publicHands[i]));
-		evalHand(table, &table->publicHands[i], 1);
+		showHand(&(table->hands[i]));
+		evalHand(table, &(table->hands[i]), 1);
 	}
 	printf("\n");
+	usleep(sleeptime);
 }
